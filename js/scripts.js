f8b22902a4c56c5ded4c76862bf561c341f8af48
Doodle6660 <ziyadzidane69@gmail.com>
2021-01-30 20:59:13 +0700

    format

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@
 const selectElement = (s) => document.querySelector(s);
 
 // Function Open Menu
-selectElement('.open').addEventListener('click', () => {
-    selectElement('.nav-list').classList.add('active');
+selectElement(".open").addEventListener("click", () => {
+  selectElement(".nav-list").classList.add("active");
 });
 // Function Close Menu
-selectElement('.close').addEventListener('click', () => {
-    selectElement('.nav-list').classList.remove('active');
+selectElement(".close").addEventListener("click", () => {
+  selectElement(".nav-list").classList.remove("active");
 });
